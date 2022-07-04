a6e3688b5ac43fe26d46acd1c9b9d00be212463a
Leo_Amv <leonidamvrosov@yandex.ru>
2022-07-05 03:10:50 +0500

    Task 1 completed!

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,27 @@
-﻿s
+﻿namespace Homework_Seminar4
+{
+    class Task25 //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+    {
+        public static double Exponentiation(int a, uint b) // Метод возведения в степень
+        {
+            double s = Math.Pow(a, b);
+            return s;
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int A;
+                uint B; // uint так как возводим в натуральную степень, а отрицательное число не может быть натуральным
+                for (Console.Clear();;Console.WriteLine("Enter number A"), A = Convert.ToInt32(Console.ReadLine()), Console.WriteLine("Enter number B"), B = Convert.ToUInt32(Console.ReadLine()), Console.WriteLine("Result = " + Exponentiation(A, B)))
+                {
+                    Console.WriteLine();
+                }                
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect data try entering a positive number B or do not enter letters!");
+            }
+        }
+    }
+}
